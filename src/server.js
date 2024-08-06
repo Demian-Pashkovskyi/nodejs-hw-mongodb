ebf007a9a026fc89bc345db93cb6a1c5b81602d5
Demian Pashkovskyi <95377702+Demian-Pashkovskyi@users.noreply.github.com>
2024-08-06 20:09:49 +0300

    node_modules

--- src/server.js
+++ src/server.js
@@ -25,8 +25,8 @@ export const setupServer = () => {
     const contacts = await getAllContacts();
     res.status(200).json({
       status: 200,
+			message: 'Successfully found contacts!',
       data: contacts,
-      message: 'Successfully found contacts!',
     });
   });
 
@@ -52,8 +52,8 @@ export const setupServer = () => {
 
     res.status(200).json({
       status: 200,
+			message: `Successfully found contact with id ${contactId}!`,
       data: contact,
-      message: `Successfully found contact with id ${contactId}!`,
     });
   });
 
